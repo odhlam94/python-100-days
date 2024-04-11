a96e2e2ee91a6acfc682573fd81ce7a2ccedeec5
lamdoinfodation <lam.do@infodation.vn>
2024-04-11 14:37:36 +0700

    Init pong game project

---
+++ day_21_pong_game/main.py
@@ -0,0 +1,4 @@
+from models.game import Game
+
+game = Game()
+game.start()
---
+++ day_21_pong_game/models/game.py
@@ -0,0 +1,8 @@
+from shared import screen
+
+class Game:
+    def __init__(self):
+        pass
+
+    def start(self):
+        screen.exitonclick()
